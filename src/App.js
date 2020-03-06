bace5746724524b92358b84ad88bba0fbf7d1aed
adypamungkas <madypamungkas@gmail.com>
2020-03-06 16:36:03 +0700

    profil card - first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Card = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class={props.color}>
+    <figcaption>
+      <h2>{props.name} <span>{props.lastName}</span></h2>
+  <p>{props.desc}</p>
+      <div class="icons"><a href="#"><i class="ion-ios-home"></i></a><a href="#"><i class="ion-ios-email"></i></a><a href="#"><i class="ion-ios-telephone"></i></a></div>
+    </figcaption><img src={props.img} alt="sample9" />
+    <div class="position">{props.jobs}</div>
+  </div>
+   );
+}
+const App = () =>{
+  return(
+<div>
+    <Card name = "Name "
+          lastName="Last"
+          desc="Deskripsi deskripsi deskripsi deskripsi deskripsi deskripsi deskripsi deskripsi "
+          img="https://solve.technow.id/storage/user/14" 
+          color="card blue" 
+          jobs="Job 1"  />
+   
+  
+   </div>
   );
 }
 
