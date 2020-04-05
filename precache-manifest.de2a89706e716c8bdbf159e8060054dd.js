6d070f0ed5f0991e94b6e252654573c92bfca904
Ady Pamungkas <madypamungkas@gmail.com>
2020-04-05 11:15:04 +0700

    Updates

---
+++ precache-manifest.de2a89706e716c8bdbf159e8060054dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93a69a94256e5804832c63b0cf47fc0",
+    "url": "/profile-card-reactjs/index.html"
+  },
+  {
+    "revision": "a5a5710c1d3441ed71d4",
+    "url": "/profile-card-reactjs/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "a494553c6f799918cb1a",
+    "url": "/profile-card-reactjs/static/css/main.8d297d92.chunk.css"
+  },
+  {
+    "revision": "a5a5710c1d3441ed71d4",
+    "url": "/profile-card-reactjs/static/js/2.e8a9cd2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/profile-card-reactjs/static/js/2.e8a9cd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a494553c6f799918cb1a",
+    "url": "/profile-card-reactjs/static/js/main.42c04414.chunk.js"
+  },
+  {
+    "revision": "6a830bbdcd67bc9e969b",
+    "url": "/profile-card-reactjs/static/js/runtime-main.876be07f.js"
+  }
+]);
